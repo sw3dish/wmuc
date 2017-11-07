4ca48e2c28cbc9f64cb337691af0f9b3430cc30c
Colin Burr <colin.a.burr@gmail.com>
2017-11-06 21:23:21 -0500

    Add to master __init__ file

--- app/wmuc/__init__.py
+++ app/wmuc/__init__.py
@@ -1,4 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
-import wmuc.controllers.hello
+app = Flask(__name__)
+app.config.from_object("wmuc.config")
+
+import wmuc.database
+
+import wmuc.controllers.about
+import wmuc.controllers.home
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
