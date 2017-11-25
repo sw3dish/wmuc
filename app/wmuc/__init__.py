cac790225c36af6048adb718c2c683b827abe223
Colin Burr <colin.a.burr@gmail.com>
2017-11-24 21:10:56 -0500

    Add packages for users
    Adds form, login support for users, as well as SQLAlchemy and Alembic
    for database work

--- app/wmuc/__init__.py
+++ app/wmuc/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object("wmuc.config")
+db = SQLAlchemy(app)
 
 import wmuc.database
 
 import wmuc.controllers.about
 import wmuc.controllers.home
+import wmuc.controllers.login
 
 @app.errorhandler(404)
 def page_not_found(e):
--- app/wmuc/config.py
+++ app/wmuc/config.py
@@ -1,2 +1,8 @@
-SQLALCHEMY_DATABASE_URI='postgresql://wmuc:wmuc881@localhost:5432/wmuc'
 DEBUG=True
+
+#SQLAlchemy
+SQLALCHEMY_DATABASE_URI='postgresql://wmuc:wmuc881@localhost:5432/wmuc'
+
+# WTForms
+WTF_CSRF_ENABLED=True
+SECRET_KEY = 'bigbrothersamisalwayswatching'
